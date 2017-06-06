8862bf6bdda6f344d822698a0a289e9487d54416
julianlerry <lerryjulian@yahoo.fr>
2017-06-06 12:04:38 +0100

    Database components updating.

--- app/src/main/java/ga/astech/mbaya/dao/DBHelper.java
+++ app/src/main/java/ga/astech/mbaya/dao/DBHelper.java
@@ -1,8 +1,19 @@
 package ga.astech.mbaya.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by julian on 6/5/17.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "Mbaya.db";
+    public static final int DB_VERSION = 1;
+
+    public DBHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION );
+    }
 }
